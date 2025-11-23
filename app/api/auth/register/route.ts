882ca421b6c6e1c22dc9bbee794b7da3fcbd3d8e
Joerakesh <rakeshjoe52@gmail.com>
2025-11-23 23:07:34 +0530

    registration

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,14 +1,36 @@
-import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
+import connectDB from "@/app/api/db/connection";
 import User from "@/lib/models/User";
-import { connectDB } from "../../db/connection";
+import bcrypt from "bcryptjs";
 
-export async function POST(request: Request) {
+export async function POST(req: Request) {
   await connectDB();
-  const { name, email, password } = await request.json();
+
+  const { name, email, password, role, department } = await req.json();
+
+  if (!name || !email || !password) {
+    return Response.json(
+      { success: false, error: "Missing fields" },
+      { status: 400 }
+    );
+  }
+
+  const exists = await User.findOne({ email });
+  if (exists) {
+    return Response.json(
+      { success: false, error: "User already exists" },
+      { status: 400 }
+    );
+  }
 
   const hashed = await bcrypt.hash(password, 10);
-  await User.create({ name, email, password: hashed });
 
-  return NextResponse.json({ success: true });
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+    role,
+    department,
+  });
+
+  return Response.json({ success: true, user }, { status: 201 });
 }
