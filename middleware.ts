501c3470958e7ef502c61b4cb9c7ea86488359dc
Joerakesh <rakeshjoe52@gmail.com>
2025-11-23 23:04:08 +0530

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,48 @@
+export const runtime = "nodejs";
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const path = req.nextUrl.pathname;
+
+  const publicPaths = ["/login", "/", "/api/auth/login"];
+
+  if (publicPaths.includes(path)) return NextResponse.next();
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (path.startsWith("/admin") && decoded.role !== "admin") {
+      return NextResponse.redirect(new URL("/unauthorized", req.url));
+    }
+
+    if (path.startsWith("/faculty") && decoded.role !== "faculty") {
+      return NextResponse.redirect(new URL("/unauthorized", req.url));
+    }
+
+    if (path.startsWith("/student") && decoded.role !== "student") {
+      return NextResponse.redirect(new URL("/unauthorized", req.url));
+    }
+
+    return NextResponse.next();
+  } catch (err) {
+    console.log("JWT Verify Error:", err);
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/admin/:path*",
+    "/faculty/:path*",
+    "/student/:path*",
+    "/judge/:path*",
+  ],
+};
