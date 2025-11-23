2d261a70b80b5786d0f61cf7c821c8dc539343da
Joerakesh <rakeshjoe52@gmail.com>
2025-11-23 16:59:23 +0530

    Teams Data in TS

---
+++ data/teams.ts
@@ -0,0 +1,23 @@
+export const shiftOne = [
+  "Botany",
+  "Chemistry",
+  "Commerce",
+  "Computer Science",
+  "Economics",
+  "English",
+  "HRM",
+  "History",
+  "Mathematics",
+  "Physics",
+  "Statistics",
+  "Tamil",
+];
+
+export const shiftTwo = [
+  "Bio-Chemistry & Bio-Technology",
+  "Business Administration (BBA)",
+  "Commerce CA",
+  "Data Sci. & Chemistry",
+  "Commerce",
+  "Counselling Psychology",
+];
