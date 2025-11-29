edb29f6d86c281250afa4de954c97241ec06e97c
Joerakesh <rakeshjoe52@gmail.com>
2025-11-29 15:22:32 +0530

    added condition to only update the counter from pc only for 15mins

---
+++ app/api/track-count/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import Visitor from "@/lib/models/Visitor";
+import connectDB from "@/app/api/db/connection";
+
+export async function GET() {
+  await connectDB();
+  let counter = await Visitor.findOne();
+  if (!counter) counter = await Visitor.create({ total: 0 });
+
+  return NextResponse.json({ total: counter.total });
+}
