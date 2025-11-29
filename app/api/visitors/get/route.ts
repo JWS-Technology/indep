a03bdcf21b45914605bc80e2097a883ef04d35f5
Joerakesh <rakeshjoe52@gmail.com>
2025-11-29 15:19:13 +0530

    deleted visitor count function - old

--- app/api/visitors/get/route.ts
+++
@@ -1,9 +0,0 @@
-import connectDB from "@/app/api/db/connection";
-import Visitor from "@/lib/models/Visitor";
-
-export async function GET() {
-    await connectDB();
-
-    const record = await Visitor.findOne();
-    return Response.json({ count: record?.count || 0 });
-}
