fe69f6feae7c2aba1c258d597b6206185810b6de
Joerakesh <rakeshjoe52@gmail.com>
2025-11-30 00:13:13 +0530

    new sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,19 @@ import Hero from '@/components/Hero'
 import Stats from '@/components/Stats'
 import SectionTitle from '@/components/SectionTitle'
 import About from '@/components/About'
+import EventSection from '@/components/EventSection'
+import TeamSection from '@/components/TeamSection'
 
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      {/* <SectionTitle /> */}
       <About />
-      <Stats />
-
+      <TeamSection />
+      <EventSection />
+      {/* <Stats /> */}
+      {/* <SectionTitle /> */}
     </div>
   )
 }
