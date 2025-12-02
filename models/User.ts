61b5fa0ad5fbabe783d08ee32d48be4056cb8c37
Noel Sebu <noelsebu555@gmail.com>
2025-12-02 21:55:16 +0530

    two new enums added

--- models/User.ts
+++ models/User.ts
@@ -17,7 +17,7 @@ const UserSchema = new Schema(
     },
     role: {
       type: String,
-      enum: ["admin", "faculty", "student", "judge", "department"],
+      enum: ["admin", "faculty", "student", "judge", "department", "president", "secretary"],
       required: true,
     },
     department: {
