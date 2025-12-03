1fb5aa8036d29c68533a81018cdd9bfa863909d3
Noel Sebu <noelsebu555@gmail.com>
2025-12-03 10:16:26 +0530

    lot model created

---
+++ models/Lot.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const LotSchema = new mongoose.Schema({
+  lot_number: {
+    type: String,
+    required: true,
+  },
+  event: {
+    type: String,
+    required: true,
+  },
+  team_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Team",
+    required: true,
+  },
+  theme: {
+    type: String,
+    default: "", // keep empty by default
+  },
+});
+
+export default mongoose.model("Lot", LotSchema);
