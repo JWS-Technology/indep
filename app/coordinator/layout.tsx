2ac5e804916c1a8be32aa765c4d14243b8fbe6f7
Joerakesh <rakeshjoe52@gmail.com>
2025-12-04 10:39:58 +0530

    fixed layout

--- app/coordinator/layout.tsx
+++ app/coordinator/layout.tsx
@@ -2,14 +2,14 @@
 
 import CoordinatorSidebar from "@/components/Coordinator/CoordinatorSidebar";
 
-export default function coordinatorLayout({ children }: { children: React.ReactNode }) {
+export default function CoordinatorLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex">
+        <div className="flex min-h-screen bg-slate-50">
             {/* Sidebar */}
             <CoordinatorSidebar />
 
             {/* Content Area */}
-            <main className="flex-1 ml-64 min-h-screen bg-gray-50 p-10">
+            <main className="flex-1 ml-64 p-8 md:p-12 transition-all duration-300 ease-in-out">
                 {children}
             </main>
         </div>
