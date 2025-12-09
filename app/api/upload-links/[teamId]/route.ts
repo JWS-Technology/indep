7aa7703c878a3a25d464c9f8fbd14b808b83df4a
Joerakesh <rakeshjoe52@gmail.com>
2025-12-10 00:23:55 +0530

    inserting data to collection api

---
+++ app/api/upload-links/[teamId]/route.ts
@@ -0,0 +1,32 @@
+import UploadLinks from "@/models/UploadLinks";
+import dbConnect from "@/utils/dbConnect";
+
+export async function GET(
+  req: Request,
+  context: { params: { teamId: string } }
+) {
+  try {
+    await dbConnect();
+
+    const { teamId } = await context.params;
+
+    const link = await UploadLinks.findOne({ teamId });
+
+    return Response.json(
+      {
+        success: true,
+        drivelink: link?.drivelink || null,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log("Error fetching drive link:", err);
+    return Response.json(
+      {
+        success: false,
+        message: "failed to fetch link",
+      },
+      { status: 500 }
+    );
+  }
+}
