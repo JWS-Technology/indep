569721ee4bcfb4702afa5188091d17fcf29ac0be
Noel Sebu <noelsebu555@gmail.com>
2025-12-10 15:45:10 +0530

    bankend route to get bank details creaeted

---
+++ app/api/admin/get-bank-details/route.ts
@@ -0,0 +1,33 @@
+import BankDetails from "@/models/BankDetails";
+import dbConnect from "@/utils/dbConnect";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    await dbConnect();
+
+    const savedDetails = await BankDetails.find();
+
+    console.log("this is saved bank deails");
+    console.log(savedDetails);
+
+    return NextResponse.json(
+      {
+        message: "got bank details successfully",
+        success: true,
+        bankDetails: savedDetails,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error in save bank details route");
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "failed to save bank details",
+        success: false,
+      },
+      { status: 400 }
+    );
+  }
+}
